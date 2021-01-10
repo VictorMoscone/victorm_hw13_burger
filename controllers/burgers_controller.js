001e12aa72a921ffb1cf25e184e545446adc8065
Victor Moscone <victormoscone@gmail.com>
2021-01-09 16:13:31 -0800

    Con Feat: Basics of routing setup.

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const burger = require ('../models/burger');
+const router = express.Router();
+
+app.get('/', (req, res) => {
+
+});
+
+module.exports = router;
