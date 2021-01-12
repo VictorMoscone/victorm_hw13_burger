4d286f050a4778b9648e009fbc3f0bc9aaf0c3fe
Victor Moscone <victormoscone@gmail.com>
2021-01-11 17:47:13 -0800

    Cont. Feat: Setup our router's get request.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,8 +2,15 @@ const express = require('express');
 const burger = require ('../models/burger');
 const router = express.Router();
 
-app.get('/', (req, res) => {
-
+router.get('/', (req, res) => {
+    // This will run the selectAll query for the burger db.
+    burger.selectAll((data) => {
+        // This well render all Burgers table data using handlebars.
+        const burgerBar = {
+          burgers: data,
+        };
+        res.render('index', burgerBar);
+      });
 });
 
 module.exports = router;
